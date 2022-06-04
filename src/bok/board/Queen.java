bb61e64c208f1b8f5902d4ad5dd8c1d7183b05b5
DidntRead <25854753+DidntRead@users.noreply.github.com>
2022-06-04 09:26:07 +0300

    queen piece

--- src/bok/board/Queen.java
+++ src/bok/board/Queen.java
@@ -4,25 +4,31 @@ import bok.engine.board.interfaces.Board;
 import bok.engine.board.interfaces.Piece;
 import bok.engine.game2d.Move;
 import bok.engine.game3d.util.obj.ModelData;
+import bok.engine.game3d.util.obj.OBJFileLoader;
+import bok.engine.game3d.util.obj.OBJLoader;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.List;
+import java.util.Objects;
 
 public class Queen extends Piece {
     public Queen(Board board, Color color, int player) {
         super(board, color, player);
+        getForm().setScale(0.5f);
+        getForm().setRotation((float) Math.toRadians(90), 0, 0);
     }
 
     @Override
     protected BufferedImage loadImage() throws IOException {
-        return null;
+        return ImageIO.read(Objects.requireNonNull(Queen.class.getResourceAsStream("/black-queen.png")));
     }
 
     @Override
     protected ModelData loadModelData() {
-        return null;
+        return OBJFileLoader.loadOBJ("/queen.obj");
     }
 
     @Override
